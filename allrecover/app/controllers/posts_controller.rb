b8750caac617f813c6f6b572a1c240c04482c66d
habaak <joohn728@gmail.com>
2018-07-12 07:30:52 +0000

    ADD Bootstrap

--- allrecover/app/controllers/posts_controller.rb
+++ allrecover/app/controllers/posts_controller.rb
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html {redirect_to '/', notice: "글 작성완료"}
+            #notice는 flash[:notice]에 값을 담기 위해서
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
--- allrecover/app/models/post.rb
+++ allrecover/app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: {message: "제목을 입력해주세요."},
+                    length: {maximum: 30,
+                            too_long: "제목은 %{count}자 이내로 입력해주세요." }
+  validates :content, presence: {message: "내용을 입력해주세요."}
 end
